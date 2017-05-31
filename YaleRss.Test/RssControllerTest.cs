00df97180c86f08690787536b631fa3c91956adb
Denis Palnitsky <palnitsky@gmail.com>
2017-05-31 22:48:36 +0200

    Test project added

---
+++ YaleRss.Test/RssControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using YaleRSS.Controllers;
+
+namespace YaleRss.Test
+{
+    [TestFixture]
+    public class RssControllerTest
+    {
+        [Test]
+        public void Test()
+        {
+            RssController controller = new RssController();
+            var d = controller.Get();
+            DataContractSerializer ser = new DataContractSerializer(d.GetType());
+            
+            Assert.DoesNotThrow(() => ser.WriteObject(new MemoryStream(), d) );
+        }
+    }
+}
