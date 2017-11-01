8eb809379692002046df3e0f34066a0cd8658493
Denis Palnitsky <palnitsky@gmail.com>
2017-11-01 23:25:48 +0100

    New structure of Lecture data entity

--- YaleRSS.Data/LectureEntity.cs
+++ YaleRSS.Data/LectureEntity.cs
@@ -1,19 +1,11 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace YaleRSS.Data
+﻿namespace YaleRSS.Data
 {
     public class LectureEntity
-    {
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+    {      
         public int  Order { get; set; } 
         public string Name { get; set; }
         public string LectureId { get; set; }
+        public string LectureNumber { get; set; }
+        public string Overview { get; set; }
     }
 }
