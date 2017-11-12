56cd793b00c60cdab5a3b79ecc51f06077cff282
Denis_Palnitsky <dpalnitsky@corelogic.com>
2017-11-12 21:20:29 +0100

    Disable application insights

--- YaleRSS/App_Start/FilterConfig.cs
+++ YaleRSS/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace YaleRSS
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
+            //filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
