f8d85cedd677bb83351186db67edcdf5fe172f37
Denis_Palnitsky <dpalnitsky@corelogic.com>
2017-11-12 22:15:12 +0100

    TraceExceptionLogger added

---
+++ YaleRSS/ErrorHandler/TraceExceptionLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.ExceptionHandling;
+
+namespace YaleRSS.ErrorHandler
+{
+    public class TraceExceptionLogger : IExceptionLogger
+    {
+       
+        public Task LogAsync(ExceptionLoggerContext context,
+                      CancellationToken cancellationToken)
+        {
+            return Task.Run(() => Trace.WriteLine(context.Exception.ToString()), cancellationToken);
+        }
+    }
+}
