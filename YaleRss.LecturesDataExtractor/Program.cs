112c404cf08f0ae68435b01634ecaf8038b837c5
Denis_Palnitsky <dpalnitsky@corelogic.com>
2017-11-12 17:30:12 +0100

    Function to download files from Yale site

--- YaleRss.LecturesDataExtractor/Program.cs
+++ YaleRss.LecturesDataExtractor/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using YaleRSS.Data;
+using YaleRSS.Data.WebData;
 
 namespace YaleRss.LecturesDataExtractor
 {
@@ -40,30 +43,51 @@ namespace YaleRss.LecturesDataExtractor
             "http://oyc.yale.edu/philosophy/phil-181/lecture-26"
             };
 
+            var links =  GetAllLinksToMedia(urls);
 
+            foreach (var l in links)
+            {
+                var path = Path.Combine(Environment.CurrentDirectory, Path.GetFileName(l.Substring(l.IndexOf(@"/mp3/"))));
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+                using (FileStream f = new FileStream(path, FileMode.Create))
+                {
+                    YaleSiteRequest.GetFile(l).GetResponseStream().CopyTo(f);
+                }
+            }
+        }
+
+        private static string[] GetAllLinksToMedia(System.Collections.Generic.List<string> urls)
+        {
             HttpClient http = new HttpClient();
 
             if (File.Exists(OUTPUT_FILE_NAME))
                 File.Delete(OUTPUT_FILE_NAME);
 
-            foreach (var url in urls) {
+            List<string> links =  new List<string>(); 
+
+            foreach (var url in urls)
+            {
 
                 var t = http.GetStringAsync(url);
                 t.Wait();
 
                 var r = t.Result;
 
-                var result = new {
+                var result = new
+                {
                     Overview = StringBetweenTwo(r, @"Overview</h3>", "</p>"),
                     LectureId = StringBetweenTwo(r, @"media_downloader.cgi?file=/courses/spring11/phil181/mp3/", ".mp3"),
-                    LectureNumber = StringBetweenTwo(r,@"session-number"">", "</h2>"),
-                    Name = StringBetweenTwo (r, @"session-title"">&nbsp;-&nbsp;", "</h2>")
+                    LectureNumber = StringBetweenTwo(r, @"session-number"">", "</h2>"),
+                    Name = StringBetweenTwo(r, @"session-title"">&nbsp;-&nbsp;", "</h2>")
                 };
 
                 File.AppendAllText(OUTPUT_FILE_NAME, JsonConvert.SerializeObject(result) + ", \n");
-
+                links.Add($"http://openmedia.yale.edu/cgi-bin/open_yale/media_downloader.cgi?file=/courses/spring11/phil181/mp3/{ result.LectureId }.mp3");
                 Console.WriteLine(JsonConvert.SerializeObject(result));
             }
+
+            return links.ToArray();
         }
 
         public static string StringBetweenTwo ( string str, string before, string after)
@@ -73,5 +97,6 @@ namespace YaleRss.LecturesDataExtractor
 
             return str.Substring(start, length);
         }
+
     }
 }
