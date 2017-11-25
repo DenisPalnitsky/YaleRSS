822e5441bd21f7260541aa3814d7eeac05077838
Denis Palnitsky <palnitsky@gmail.com>
2017-11-26 00:03:29 +0100

    Use redirect to Azure storage

--- YaleRSS/Controllers/LecturesController.cs
+++ YaleRSS/Controllers/LecturesController.cs
@@ -20,27 +20,11 @@ namespace YaleRSS.Controllers
             Trace.WriteLine($"Downloading lecture { id }");
             var course = _repo.Philosophy;
             var lecture = course.Lectures.Single(l => l.LectureId.Equals(id, StringComparison.CurrentCultureIgnoreCase));
-          
-            var yaleResponse = YaleRSS.Data.WebData.YaleSiteRequest.GetFile(String.Format(course.AudioUrlPattern, lecture.LectureId));
-            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
-
-            if (yaleResponse.StatusCode == HttpStatusCode.OK)
-            {
-                Trace.WriteLine("Attempt to get stream from Yale succeeded");
-                response.Content = new StreamContent(yaleResponse.GetResponseStream());             
-            }
-            else
-            {
-                Trace.WriteLine("Attempt to get stream from Yale failed. Sourcing from storage");
-                yaleResponse = YaleRSS.Data.WebData.YaleSiteRequest.GetFile(String.Format(course.AlternativeAudioUrlPattern, lecture.LectureId));
-            }
-
-            Trace.WriteLine("Setting headers");
-            response.Content.Headers.ContentType = MediaTypeHeaderValue.Parse(yaleResponse.ContentType);
-            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
-            response.Content.Headers.ContentDisposition.FileName = lecture.LectureId + ".mp3";
-            response.Content.Headers.ContentLength = yaleResponse.ContentLength;
-
+                                
+            Trace.WriteLine("Requesting file from storage");
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Moved);
+            response.Headers.Location = new Uri(String.Format(course.AlternativeAudioUrlPattern, lecture.LectureId));
+            
             Trace.WriteLine("Returning response");
             return response;
         }
