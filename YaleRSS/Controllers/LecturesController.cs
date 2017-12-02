b3772b595a82a57015fe0c853f58f3979a19cb2d
Denis Palnitsky <palnitsky@gmail.com>
2017-12-02 19:51:12 +0100

    Fixed Lectures controller

--- YaleRSS/Controllers/LecturesController.cs
+++ YaleRSS/Controllers/LecturesController.cs
@@ -8,24 +8,22 @@ using YaleRss.Data;
 
 namespace YaleRss.Controllers
 {
-    [Route("api/[controller]", Name = RouteNames.UserProfile)]
+    //[Route("api/[controller]", Name = RouteNames.UserProfile)]
     public class LecturesController : Controller
     {
         CourseRepository _repo = new CourseRepository(DbContext.Create());
 
-        [HttpGet("{id}")]
-        public HttpResponseMessage GetLectures(string id)
+        [HttpGet("api/lectures/{id}")]
+        public IActionResult GetLectures(string id)
         {            
             Trace.WriteLine($"Downloading lecture { id }");
             var course = _repo.Philosophy;
             var lecture = course.Lectures.Single(l => l.LectureId.Equals(id, StringComparison.CurrentCultureIgnoreCase));
                                 
             Trace.WriteLine("Requesting file from storage");
-            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Moved);
-            response.Headers.Location = new Uri(String.Format(course.AlternativeAudioUrlPattern, lecture.LectureId));
             
             Trace.WriteLine("Returning response");
-            return response;
+            return   RedirectPermanent(String.Format(course.AlternativeAudioUrlPattern, lecture.LectureId));
         }
 
         
