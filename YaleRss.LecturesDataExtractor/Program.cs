00db101a1aa170a4f11052871d7698e8d2229440
Denis Palnitsky <palnitsky@gmail.com>
2017-12-12 22:39:14 +0100

    Slight improvements in data extractor

--- YaleRss.LecturesDataExtractor/Program.cs
+++ YaleRss.LecturesDataExtractor/Program.cs
@@ -10,40 +10,21 @@ namespace YaleRss.LecturesDataExtractor
 {
     class Program
     {
-        private const string OUTPUT_FILE_NAME = "lectures.json";
+        private const string OUTPUT_FILE_NAME = "psyc110.json";
 
         static void Main(string[] args)
         {
-            System.Collections.Generic.List<string> urls = new System.Collections.Generic.List<string>() {
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-1",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-2",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-3",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-4",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-5",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-6",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-7",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-8",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-9",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-10",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-11",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-12",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-13",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-14",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-15",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-16",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-17",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-18",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-19",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-20",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-21",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-22",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-23",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-24",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-25",
-            "http://oyc.yale.edu/philosophy/phil-181/lecture-26"
-            };
+            System.Collections.Generic.List<string> urls = Urls.psyc_110;
 
-            var links =  GetAllLinksToMedia(urls);
+            GetMediaLinksFromLecturesLinks(urls);
+
+            Console.WriteLine("Done. Press any key to close the window");
+            Console.ReadKey();
+        }
+
+        private static void GetMediaLinksFromLecturesLinks(List<string> urls)
+        {
+            var links = GetAllLinksToMedia(urls, "spring07/psyc110");
 
             foreach (var l in links)
             {
@@ -57,7 +38,7 @@ namespace YaleRss.LecturesDataExtractor
             }
         }
 
-        private static string[] GetAllLinksToMedia(System.Collections.Generic.List<string> urls)
+        private static string[] GetAllLinksToMedia(System.Collections.Generic.List<string> urls, string lectureDirectory )
         {
             HttpClient http = new HttpClient();
 
@@ -68,23 +49,28 @@ namespace YaleRss.LecturesDataExtractor
 
             foreach (var url in urls)
             {
-
-                var t = http.GetStringAsync(url);
-                t.Wait();
-
-                var r = t.Result;
-
-                var result = new
+                try
                 {
-                    Overview = StringBetweenTwo(r, @"Overview</h3>", "</p>"),
-                    LectureId = StringBetweenTwo(r, @"media_downloader.cgi?file=/courses/spring11/phil181/mp3/", ".mp3"),
-                    LectureNumber = StringBetweenTwo(r, @"session-number"">", "</h2>"),
-                    Name = StringBetweenTwo(r, @"session-title"">&nbsp;-&nbsp;", "</h2>")
-                };
+                    var t = http.GetStringAsync(url);
+                    t.Wait();
 
-                File.AppendAllText(OUTPUT_FILE_NAME, JsonConvert.SerializeObject(result) + ", \n");
-                links.Add($"http://openmedia.yale.edu/cgi-bin/open_yale/media_downloader.cgi?file=/courses/spring11/phil181/mp3/{ result.LectureId }.mp3");
-                Console.WriteLine(JsonConvert.SerializeObject(result));
+                    var r = t.Result;
+
+                    var result = new
+                    {
+                        Overview = StringBetweenTwo(r, @"Overview</h3>", "</p>"),
+                        LectureId = StringBetweenTwo(r, $"media_downloader.cgi?file=/courses/{lectureDirectory}/mp3/", ".mp3"),
+                        LectureNumber = StringBetweenTwo(r, @"session-number"">", "</h2>"),
+                        Name = StringBetweenTwo(r, @"session-title"">&nbsp;-&nbsp;", "</h2>")
+                    };
+
+                    File.AppendAllText(OUTPUT_FILE_NAME, JsonConvert.SerializeObject(result) + ", \n");
+
+
+                    links.Add($"http://openmedia.yale.edu/cgi-bin/open_yale/media_downloader.cgi?file=/courses/{ lectureDirectory }/mp3/{ result.LectureId }.mp3");
+                    Console.WriteLine(JsonConvert.SerializeObject(result));
+                }
+                catch (ArgumentOutOfRangeException) { }
             }
 
             return links.ToArray();
---
+++ YaleRss.LecturesDataExtractor/Urls.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaleRss.LecturesDataExtractor
+{
+    class Urls
+    {
+        public  static List<string> psyc_110 = new System.Collections.Generic.List<string>() {
+                           "http://oyc.yale.edu/psychology/psyc-110/lecture-1"       ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-2"                 ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-3"                 ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-4"                 ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-5"                 ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-6"                 ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-7"                 ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-8"                 ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-9"                 ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-10"                ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-11"                ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-12"                ,
+                            "http://oyc.yale.edu/psychology/psyc-110/guest-lecture-1"           ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-13"                ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-14"                ,
+                            "http://oyc.yale.edu/psychology/psyc-110/guest-lecture-2"           ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-15"                ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-16"                ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-17"                ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-18"                ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-19"                ,
+                            "http://oyc.yale.edu/psychology/psyc-110/lecture-20"
+
+            };
+
+        public static List<string> phil_181 = new System.Collections.Generic.List<string>() {
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-1",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-2",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-3",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-4",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-5",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-6",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-7",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-8",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-9",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-10",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-11",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-12",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-13",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-14",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-15",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-16",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-17",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-18",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-19",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-20",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-21",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-22",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-23",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-24",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-25",
+            "http://oyc.yale.edu/philosophy/phil-181/lecture-26"
+            };
+
+
+}
+}
