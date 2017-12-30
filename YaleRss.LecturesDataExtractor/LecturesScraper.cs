be47aae102ae0530aa7449815428cf06d24f039f
Denis Palnitsky <palnitsky@gmail.com>
2017-12-30 22:34:35 +0100

    Lectures scraper

---
+++ YaleRss.LecturesDataExtractor/LecturesScraper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YaleRss.Data;
+using System.Linq;
+using MongoDB.Driver;
+
+namespace YaleRss.LecturesDataExtractor
+{
+    public class LecturesScraper
+    {
+        private IMongoDatabase _database;
+
+
+
+        public void ScrapeLectures()
+        {
+            var client = new MongoClient(@"mongodb://user:@ds119685.mlab.com:19685/yale_courses");
+            _database = client.GetDatabase("yale_courses");
+
+            foreach (var course in _database.GetCollection<CourseEntity>("courses").AsQueryable())
+            {
+                if (course.Lectures.Count() == 0)
+                {
+                    var lectureUrls = new List<string>();
+                    for (int i = 1; i < 30; i++)
+                    {
+                        lectureUrls.Add($"https://oyc.yale.edu/{ course.DepartmentLink }/{course.CourseIdReadeble.Replace(' ', '-')}/lecture-{i}");
+                    }
+
+                    var s = Program.GetAllLinksToMedia(lectureUrls, Program.StringBetweenTwo(course.YaleUrlPattern, "/courses/", "/mp3/{0}.mp3"));
+
+                    foreach (var l in s)
+                    {
+                        AddLecture(course, l);
+                    }
+                }
+            }
+        }
+
+        public void AddLecture(CourseEntity course, LectureEntity lecture)
+        {
+            var collection = _database.GetCollection<CourseEntity>("courses");
+            UpdateDefinition<CourseEntity> update = Builders<CourseEntity>.Update.Push(x => x.Lectures, lecture);
+            var filter = Builders<CourseEntity>.Filter.Eq(x => x.Id, course.Id);
+            collection.UpdateOne(filter, update);
+        }
+    }
+}
--- YaleRss.LecturesDataExtractor/Program.cs
+++ YaleRss.LecturesDataExtractor/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using YaleRss.Data;
 using YaleRss.Data.WebData;
@@ -14,75 +15,108 @@ namespace YaleRss.LecturesDataExtractor
 
         static void Main(string[] args)
         {
-            System.Collections.Generic.List<string> urls = Urls.psyc_110;
 
-            GetMediaLinksFromLecturesLinks(urls);
+            LecturesScraper scraper = new LecturesScraper();
+            scraper.ScrapeLectures();
 
             Console.WriteLine("Done. Press any key to close the window");
             Console.ReadKey();
         }
 
-        private static void GetMediaLinksFromLecturesLinks(List<string> urls)
+
+        public static void GetLinksFromUrl()
         {
-            var links = GetAllLinksToMedia(urls, "spring07/psyc110");
-
-            foreach (var l in links)
-            {
-                var path = Path.Combine(Environment.CurrentDirectory, Path.GetFileName(l.Substring(l.IndexOf(@"/mp3/"))));
-                Directory.CreateDirectory(Path.GetDirectoryName(path));
-
-                using (FileStream f = new FileStream(path, FileMode.Create))
-                {
-                    YaleSiteRequest.GetFile(l).GetResponseStream().CopyTo(f);
-                }
-            }
+            var l = AllStringBetweenTwo("a cc ba cc b", "a", "b").ToList();
+            Console.WriteLine(string.Join(" ",  l));
         }
 
-        private static string[] GetAllLinksToMedia(System.Collections.Generic.List<string> urls, string lectureDirectory )
+        //private static void GetMediaLinksFromLecturesLinks(List<string> urls)
+        //{
+        //    var links = GetAllLinksToMedia(urls, "spring07/psyc110");
+
+        //    foreach (var l in links)
+        //    {
+        //        var path = Path.Combine(Environment.CurrentDirectory, Path.GetFileName(l.Substring(l.IndexOf(@"/mp3/"))));
+        //        Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+        //        using (FileStream f = new FileStream(path, FileMode.Create))
+        //        {
+        //            YaleSiteRequest.GetFile(l).GetResponseStream().CopyTo(f);
+        //        }
+        //    }
+        //}
+
+        public static LectureEntity[] GetAllLinksToMedia(System.Collections.Generic.List<string> urls, string lectureDirectory )
         {
             HttpClient http = new HttpClient();
 
-            if (File.Exists(OUTPUT_FILE_NAME))
-                File.Delete(OUTPUT_FILE_NAME);
+            //if (File.Exists(OUTPUT_FILE_NAME))
+            //    File.Delete(OUTPUT_FILE_NAME);
 
-            List<string> links =  new List<string>(); 
-
+            List<LectureEntity> lectures =  new List<LectureEntity>();
+            int order  = 1;
             foreach (var url in urls)
             {
                 try
                 {
                     var t = http.GetStringAsync(url);
                     t.Wait();
-
+                 
                     var r = t.Result;
 
-                    var result = new
+                    var result = new LectureEntity
                     {
                         Overview = StringBetweenTwo(r, @"Overview</h3>", "</p>"),
                         LectureId = StringBetweenTwo(r, $"media_downloader.cgi?file=/courses/{lectureDirectory}/mp3/", ".mp3"),
                         LectureNumber = StringBetweenTwo(r, @"session-number"">", "</h2>"),
-                        Name = StringBetweenTwo(r, @"session-title"">&nbsp;-&nbsp;", "</h2>")
+                        Name = StringBetweenTwo(r, @"session-title"">&nbsp;-&nbsp;", "</h2>"),
+                        Order = order
                     };
 
                     File.AppendAllText(OUTPUT_FILE_NAME, JsonConvert.SerializeObject(result) + ", \n");
 
-
-                    links.Add($"http://openmedia.yale.edu/cgi-bin/open_yale/media_downloader.cgi?file=/courses/{ lectureDirectory }/mp3/{ result.LectureId }.mp3");
-                    Console.WriteLine(JsonConvert.SerializeObject(result));
+                    order++;
+                    //lectures.Add($"http://openmedia.yale.edu/cgi-bin/open_yale/media_downloader.cgi?file=/courses/{ lectureDirectory }/mp3/{ result.LectureId }.mp3");
+                    lectures.Add(result);
                 }
                 catch (ArgumentOutOfRangeException) { }
+                //catch (HttpRequestException)
+                catch (AggregateException ex) when (ex.InnerExceptions[0] is System.Net.Http.HttpRequestException)
+                {
+                    break;
+                }
+                catch (InvalidOperationException)
+                {
+                    return new LectureEntity[0];
+                }
             }
 
-            return links.ToArray();
+            return lectures.ToArray();
         }
 
-        public static string StringBetweenTwo ( string str, string before, string after)
+        public static string StringBetweenTwo(string str, string before, string after)
         {
-            var start = str.IndexOf(before)+ before.Length;
-            var length = str.Substring(start).IndexOf(after);
-
-            return str.Substring(start, length);
+            return AllStringBetweenTwo(str, before, after).Single();
         }
 
+        public static IEnumerable<string> AllStringBetweenTwo(string str, string before, string after)
+        {
+            int beginning = 0;
+            int start;
+            do
+            {
+                start = str.IndexOf(before, beginning);
+
+                if (start == -1)
+                    break;
+
+                var index2 = str.IndexOf(after, start + before.Length);
+                var length = index2 - start - before.Length;
+                beginning = start + length ;
+
+                yield return str.Substring(start+before.Length, length );
+            }
+            while (true);
+        }
     }
 }
