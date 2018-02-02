d02885caefb82494b2dce80c53d3e9aace3fb30b
Denis Palnitsky <palnitsky@gmail.com>
2018-02-02 11:33:51 +0100

    [Refactoring] Got rid of long methods

--- YaleRSS/Controllers/RssController.cs
+++ YaleRSS/Controllers/RssController.cs
@@ -49,47 +49,59 @@ namespace YaleRss.Controllers
 
         [HttpGet("rss/{id}", Name = RouteNames.Courses)]
         public IActionResult Get(string id)
-        {                       
-            List<SyndicationItem> items = new List<SyndicationItem>();
+        {
             var course = _repo.GetCourse(id.ToLower());
+
+            List<SyndicationItem> items = BuildRssItems(course);
+
+            var rssStream = BuildRssStream(items, course.Name);
+
+            var response = new ContentResult()
+            {
+                Content = Encoding.UTF8.GetString(rssStream),
+                StatusCode = Microsoft.AspNetCore.Http.StatusCodes.Status200OK,
+                ContentType = "text/xml"
+            };
+
+            return response;
+        }
+
+        private List<SyndicationItem> BuildRssItems(CourseEntity course)
+        {
             var lectures = course.Lectures;
 
             if (lectures == null)
                 lectures = new List<LectureEntity>();
 
+            List<SyndicationItem> items = new List<SyndicationItem>();
             foreach (var lecture in lectures)
             {
-                SyndicationItem item = new SyndicationItem()
-                {
-                    Id = $"{lecture.LectureNumber}.{lecture.Name}",
-                    Published = lecture.DateOfLecture, 
-                    Title = lecture.Name,
-                    Description = lecture.Overview,
-                };
-
-                item.AddLink ( new SyndicationLink(GetAudioUri(course.CourseId, lecture)) { MediaType = "audio/mpeg" } );
-                item.AddCategory(new SyndicationCategory("Education"));
-                item.LastUpdated = lecture.DateOfLecture;
-          
+                SyndicationItem item = CreateSyndicationItem(course.CourseId, lecture);
                 items.Add(item);
             }
 
-   
-            var formatter = new RssFormatter();            
-            var xws = new XmlWriterSettings { Indent = true, Encoding = Encoding.UTF8, Async = true };            
+            return items;
+        }
 
-            using (MemoryStream ms = new MemoryStream())
+        private byte[] BuildRssStream(List<SyndicationItem> items, string feedTitle)
+        {
+            using (var ms = new MemoryStream())
             {
+                var formatter = new RssFormatter();
+                var xws = new XmlWriterSettings { Indent = true, Encoding = Encoding.UTF8, Async = true };
                 using (var xmlWriter = XmlWriter.Create(ms, xws))
                 {
                     RssFeedWriter feedWriter = new RssFeedWriter(xmlWriter);
                     feedWriter.WriteDescription("Lectures from Open Yale Courses. More info on http://oyc.yale.edu/ ");
-                    feedWriter.WriteCopyright("Most of the lectures and course material within Open Yale Courses are licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 license. ");                    
-                    feedWriter.WriteTitle($"Yale Open lectures. {course.Name}");
+                    feedWriter.WriteCopyright("Most of the lectures and course material within Open Yale Courses are licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 license. ");
+                    feedWriter.WriteTitle($"Yale Open lectures. {feedTitle}");
                     feedWriter.WriteLastBuildDate(DateTime.Now);
 
-                    var sendycationImage = new SyndicationImage(this.Url.AbsoluteContent(@"~/images/square_logo_large.jpg")) { Title = "YaleLogo" ,
-                    Link = new SyndicationLink( this.Url.AbsoluteContent(@"~/images/square_logo_large.jpg"))                    };
+                    var sendycationImage = new SyndicationImage(this.Url.AbsoluteContent(@"~/images/square_logo_large.jpg"))
+                    {
+                        Title = "YaleLogo",
+                        Link = new SyndicationLink(this.Url.AbsoluteContent(@"~/images/square_logo_large.jpg"))
+                    };
 
 
                     feedWriter.Write(sendycationImage);
@@ -97,30 +109,38 @@ namespace YaleRss.Controllers
 
                     feedWriter.WriteLanguage(CultureInfo.GetCultureInfo("en-us"));
 
-                    foreach (var item in items) {
-                       
+                    foreach (var item in items)
+                    {
                         var content = new SyndicationContent(formatter.CreateContent(item));
 
                         var enclosureItem = new SyndicationContent("enclosure", "Custom Value");
                         enclosureItem.AddAttribute(new SyndicationAttribute("type", "audio/mpeg"));
-                        enclosureItem.AddAttribute(new SyndicationAttribute("url", item.Links.First().Uri.ToString() ));
+                        enclosureItem.AddAttribute(new SyndicationAttribute("url", item.Links.First().Uri.ToString()));
 
                         // Add custom fields/attributes
                         content.AddField(enclosureItem);
 
                         feedWriter.Write(content);
-                    }                                         
+                    }
                 }
+                return ms.ToArray();
+            }
+        }
 
-                var response = new ContentResult()
-                {
-                    Content = Encoding.UTF8.GetString(ms.ToArray()),
-                    StatusCode = Microsoft.AspNetCore.Http.StatusCodes.Status200OK,
-                    ContentType = "text/xml"
-                };
+        private SyndicationItem CreateSyndicationItem(string courseId, LectureEntity lecture)
+        {
+            SyndicationItem item = new SyndicationItem()
+            {
+                Id = $"{lecture.LectureNumber}.{lecture.Name}",
+                Published = lecture.DateOfLecture,
+                Title = lecture.Name,
+                Description = lecture.Overview,
+            };
 
-                return response;
-            }   
+            item.AddLink(new SyndicationLink(GetAudioUri(courseId, lecture)) { MediaType = "audio/mpeg" });
+            item.AddCategory(new SyndicationCategory("Education"));
+            item.LastUpdated = lecture.DateOfLecture;
+            return item;
         }
 
         private Uri GetAudioUri(string courseId, LectureEntity lecture)
